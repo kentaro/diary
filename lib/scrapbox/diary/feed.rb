f462e395f9cd333ffc17239974eca776911564c6
Kentaro Kuribayashi <kentarok@gmail.com>
2022-08-11 22:20:37 +0900

    bundle gem scrapbox-diary-feed

---
+++ lib/scrapbox/diary/feed.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "feed/version"
+
+module Scrapbox
+  module Diary
+    module Feed
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/scrapbox/diary/feed_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Scrapbox::Diary::Feed do
+  it "has a version number" do
+    expect(Scrapbox::Diary::Feed::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
